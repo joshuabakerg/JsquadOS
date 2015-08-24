2513b520ad65bc772a582fee2597f7af6a8937e3
joshuabakerg <joshuabakerg@gmail.com>
2015-08-24 21:47:01 +0200

    added /etc to the load api folders

--- bin/loadApi.lua
+++ bin/loadApi.lua
@@ -1,4 +1,10 @@
 
+local FileList = fs.list("/etc")
+for i, file in ipairs(FileList) do
+	os.loadAPI("/etc/"..file)
+	print("/etc/"..file)
+end
+
 local FileList = fs.list("/bin/api")
 for i, file in ipairs(FileList) do
 	os.loadAPI("/bin/api/"..file)
